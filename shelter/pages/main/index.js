fb1b4ad9c8ef6d9ea08fd7aabd7c0cefc8cec5ac
XeniyaMV <mehnuchka.xd@gmail.com>
2023-03-26 15:05:54 -0700

    feat: add transition and fix buttons

---
+++ shelter/pages/main/index.js
@@ -0,0 +1,8 @@
+console.log(
+    `Hi!
+    I think I did everything, but I am not sure about "при уменьшении масштаба страницы браузера или увеличении ширины страницы (>1280px) вёрстка размещается по центру, а не сдвигается в сторону и не растягивается по всей ширине"
+    The "Not only" block I called "Start screen";
+    The "Our friends" block I called "Pets";
+    The "In addition" block I called "Donation"`
+
+)
