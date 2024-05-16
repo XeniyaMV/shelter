841f0b74ce4cf0851a79fc28dfc20e29d85d375a
XeniyaMV <mehnuchka.xd@gmail.com>
2024-05-15 21:32:14 -0700

    refactor: change the implementation of the burger menu

--- shelter/pages/pets/pets_script.js
+++ shelter/pages/pets/pets_script.js
@@ -1,71 +1,71 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // *BURGER MENU VARIABLES
-        let burgerMenuIcon = document.querySelector('.menu__burger');
-        let burgerClickCounter = 0;
-        let nav = document.querySelector('nav');
-        let body = document.querySelector('body');
-        let dark = document.createElement('div');
+    // // *BURGER MENU VARIABLES
+    //     let burgerMenuIcon = document.querySelector('.menu__burger');
+    //     let burgerClickCounter = 0;
+    //     let nav = document.querySelector('nav');
+    //     let body = document.querySelector('body');
+    //     let dark = document.createElement('div');
     
     // *SLIDER VARIABLES
     
     // *BURGER MENU FUNCTIONS
-        dark.classList.add('dark-screen');
-        dark.style.cssText = `
-            background-color: black;
-            opacity: 0.35;
-            width: 100vw;
-            height: 100vh;
-            position: absolute;
-            top: 0px;
-            left: 0px;
-        `;
+        // dark.classList.add('dark-screen');
+        // dark.style.cssText = `
+        //     background-color: black;
+        //     opacity: 0.35;
+        //     width: 100vw;
+        //     height: 100vh;
+        //     position: absolute;
+        //     top: 0px;
+        //     left: 0px;
+        // `;
     
     
     
     
-        body.addEventListener('click', function(event) {
-            if (nav.classList.contains('menu__nav_small-screen') && (!event.target.closest('.menu'))) {
-                nav.classList.remove('menu__nav_small-screen');
-                burgerMenuIcon.firstElementChild.style.transform = '';
-                burgerClickCounter += 1;
-                burgerMenuIcon.style.position = 'absolute';
-                body.classList.remove('active');
-                dark.style.width='0px';
+        // body.addEventListener('click', function(event) {
+        //     if (nav.classList.contains('menu__nav_small-screen') && (!event.target.closest('.menu'))) {
+        //         nav.classList.remove('menu__nav_small-screen');
+        //         burgerMenuIcon.firstElementChild.style.transform = '';
+        //         burgerClickCounter += 1;
+        //         burgerMenuIcon.style.position = 'absolute';
+        //         body.classList.remove('active');
+        //         dark.style.width='0px';
     
-            }       
-        });
+        //     }       
+        // });
         
-        nav.addEventListener('click', function(event) {
+        // nav.addEventListener('click', function(event) {
             
-            if (event.target.tagName == 'A' && nav.classList.contains('menu__nav_small-screen')) {
-                nav.classList.remove('menu__nav_small-screen');
-                burgerMenuIcon.firstElementChild.style.transform = '';
-                burgerClickCounter += 1;
-                burgerMenuIcon.style.position = 'absolute';
-                body.classList.remove('active');
-                dark.style.width='0px';
-            }
-        });
+        //     if (event.target.tagName == 'A' && nav.classList.contains('menu__nav_small-screen')) {
+        //         nav.classList.remove('menu__nav_small-screen');
+        //         burgerMenuIcon.firstElementChild.style.transform = '';
+        //         burgerClickCounter += 1;
+        //         burgerMenuIcon.style.position = 'absolute';
+        //         body.classList.remove('active');
+        //         dark.style.width='0px';
+        //     }
+        // });
     
     
-        burgerMenuIcon.firstElementChild.addEventListener('click', function(event) {
-            burgerClickCounter += 1;
-            if (burgerClickCounter % 2 == 1) {
-                burgerMenuIcon.firstElementChild.style.transform = 'rotate(90deg)';
-                burgerMenuIcon.style.position = 'fixed';
-                nav.classList.add('menu__nav_small-screen');
-                body.classList.add('active');
-                dark.style.width = `100vw`;
-                body.prepend(dark);
-            }
-            else {
-                burgerMenuIcon.firstElementChild.style.transform = '';
-                burgerMenuIcon.style.position = 'absolute';
-                nav.classList.remove('menu__nav_small-screen');
-                body.classList.remove('active');
-                dark.style.width='0px';
-            }
-        });
+        // burgerMenuIcon.firstElementChild.addEventListener('click', function(event) {
+        //     burgerClickCounter += 1;
+        //     if (burgerClickCounter % 2 == 1) {
+        //         burgerMenuIcon.firstElementChild.style.transform = 'rotate(90deg)';
+        //         burgerMenuIcon.style.position = 'fixed';
+        //         nav.classList.add('menu__nav_small-screen');
+        //         body.classList.add('active');
+        //         dark.style.width = `100vw`;
+        //         body.prepend(dark);
+        //     }
+        //     else {
+        //         burgerMenuIcon.firstElementChild.style.transform = '';
+        //         burgerMenuIcon.style.position = 'absolute';
+        //         nav.classList.remove('menu__nav_small-screen');
+        //         body.classList.remove('active');
+        //         dark.style.width='0px';
+        //     }
+        // });
 
         function getMobilePetCard(petInfo) {
             let divPetCardContainer = document.createElement('article');
